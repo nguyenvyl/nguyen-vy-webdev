52dbdec83b4c3a919a71396f622858c726f323ab
nguyenvyl <nguyevl@yahoo.com>
2016-10-19 10:29:56 -0700

    Trying to deploy... again

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assignment/public'));
 
-require ("./test/app.js")(app);
+require ("./assignment/test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 5000;
